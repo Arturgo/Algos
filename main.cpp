ee61ab55802312ea9d3d781e836e678faad827ac
Arthur <arturgo@hotmail.fr>
2018-01-06 19:16:48 +0100

    .gitignore

--- main.cpp
+++ main.cpp
@@ -4,31 +4,10 @@
 #include <vector>
 using namespace std;
 
-vector<bool> vc;
-vector<bool> va, vb;
-
 buint a, b, c;
 
 int main() {
-	for(int i = 0;i < 1000000;i++) {
-		va.push_back(rand() % 2);
-		a.set(i, va.back());
-		vb.push_back(rand() % 2);
-		b.set(i, vb.back());
-	}
-	
-	clock_t deb = clock();
-	
-	vc.resize(1000004);
-	for(int i = 0;i < 1000000;i++) {
-		vc[i] = va[i] | vb[i];
-	}
-	
-	clock_t mil = clock();
-	
-	c = a | b;
-	
-	clock_t fin = clock();
-	
-	cout << (long long)(mil - deb) << " " << (long long)(fin - mil) << endl;
+	a = 1000000000000000;
+	b = 1000000000000000;
+	cout << (a + b).to_ulong() << endl;
 }
